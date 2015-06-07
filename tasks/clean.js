1957196ae5b8b06c553d52e3616d4c5d528a5909
Sabin Tudor <sabin@gmx.us>
2015-06-07 15:19:24 +0300

    Add Gulp clean task.

---
+++ tasks/clean.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// KATALYST global config
+var CONFIG = require('../config.js');
+
+// deps
+var gulp = require('gulp');
+var del = require('del');
+
+//	CLEAN task
+gulp.task('clean', function(cb) {
+	// del build dir
+	return del([CONFIG.paths.app.dest], cb);
+});
