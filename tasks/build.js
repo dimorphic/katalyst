c1925995cc88a26e8fe871780e2a33b534a740bb
Sabin Tudor <sabin@gmx.us>
2015-06-15 00:43:12 +0300

    Update build:dev task.

--- tasks/build.js
+++ tasks/build.js
@@ -8,10 +8,22 @@ var runSequence = require('run-sequence');
 // BUILD:DEV task
 //
 gulp.task('build:dev', ['clean'], function(cb) {
-  runSequence('copy:dependencies',
-  [
-      'css:dev',
-    //   'js:dev',
-      'copy'
-  ], cb);
+  runSequence(
+      'clear:terminal',
+      'copy:dependencies',
+      [
+          'css:dev',
+        //   'js:dev',
+          'copy'
+      ],
+      'browser:serve',
+      'watch',
+  cb);
+});
+
+//
+//  BUILD task
+//
+gulp.task('build', function(cb){
+    // @TODO
 });
