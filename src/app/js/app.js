37f6f76a7904526e353b7444fa326d370dbd61e9
Sabin Tudor <sabin@gmx.us>
2015-06-21 21:36:49 +0300

    Add Brain component to main app bootstrap.

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,11 +1,18 @@
 define(function(require){
 	'use strict';
 
+	// deps
 	var $ = require('jquery');
+	var React = require('react');
+
+	// components
+	var Brain = require('components/Brain');
 
 	// DOM ready
 	$(function() {
-		console.log('test bro');
-		// console.log(test);
+
+		console.log('test bro!');
+
+		React.render(<Brain />, document.getElementById('app'));
 	});
 });
