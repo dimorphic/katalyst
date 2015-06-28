faa434121f4da05384f0bfa68775b30a4351d81d
Sabin Tudor <sabin@gmx.us>
2015-06-28 21:34:11 +0300

    Add JS:BUNDLE task to compile JS files into 1 main file via RequireJS builder. Also copies vendor lib to build dist/ dir.

--- tasks/js.js
+++ tasks/js.js
@@ -12,6 +12,9 @@ var runSequence = require('run-sequence');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 
+// JS package manager / bundler
+var requireJS = require('gulp-requirejs');
+
 //
 //	JS:TRANSFORM
 //	do any transforms here (es 6-to-5, react, etc)
@@ -31,7 +34,7 @@ gulp.task('js:transform', function() {
 });
 
 //
-//	JS:MAKE task (aka 'compile')
+//	JS:COPY task
 //
 gulp.task('js:copy', function() {
 	// stream I/O
@@ -47,13 +50,50 @@ gulp.task('js:copy', function() {
 });
 
 //
+//	JS:BUNDLE task (aka 'compile' task)
+//
+gulp.task('js:bundle', function() {
+	// bundler settings
+	var requireConfig = {
+        name: 'main',
+		out: 'main.js',
+		optimize: 'none',
+		baseUrl: CONFIG.paths.js.temp,
+        mainConfigFile: CONFIG.paths.js.temp + 'main.js'
+    };
+
+	// stream I/O
+	var input = CONFIG.paths.js.temp + 'vendor/require.js';
+	var output = CONFIG.paths.js.dest;
+
+	// copy requirejs vendor lib
+	var requireLib = gulp.src(input)
+		.pipe(gulp.dest(output + 'vendor/'));
+
+	// go bundle stuff!
+    requireJS(requireConfig)
+		.pipe(uglify())
+		.pipe(gulp.dest(output));
+});
+
+//
 //	JS:DEV task
 //
 gulp.task('js:dev', function(cb) {
 	runSequence(
 		'eslint',
 		'js:transform',
-		// 'requirejs',
 		'js:copy',
+	cb);
+});
+
+//
+//	JS:PROD task
+//
+gulp.task('js:prod', function(cb) {
+	runSequence(
+		'eslint',
+		'js:transform',
+		'js:bundle',
 	cb);
 });
