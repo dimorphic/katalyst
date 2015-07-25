191c89f6306c7d68efcf5fdcd231bc723de02886
Sabin Tudor <sabin@gmx.us>
2015-07-25 16:53:12 +0300

    Sorting things into mixing. #wip

--- src/app/js/components/Brain.jsx
+++ src/app/js/components/Brain.jsx
@@ -13,115 +13,13 @@ define(function(require){
 	var Braincell = require('components/Braincell');
 
 	//
-	// Brain component
+	//	Animation MIXIN
 	//
-	var Brain = React.createClass({
-		getInitialState: function() {
-			return {
-				// cells size
-				cellsSize: 25,
-
-				brainCells: []
-			};
-		},
-
-		componentWillMount: function() {
-			// get cells max count
-			this.anim.cellsCount = Memory.getCellsCount(this.state.cellsSize);
-
-			this.buildMemory();
-
-			// randomize memory activity
-			if (this.anim.randomizeMemory) {
-				setInterval(function() {
-					this.buildNoiseMap(this.state.brainCells);
-				}.bind(this), 7000);
-			}
-		},
-
-		componentDidMount: function() {
-			// handle window resize
-			window.addEventListener('resize', this.onScreenResize);
-
-			// start anim (rebuild memory)
-			if (this.anim.useRAF) {
-				this.redraw();
-			} else {
-				this.anim.updateTimer = setInterval(this.redraw, this.anim.updateDelay);
-			}
-		},
-
-		onScreenResize: function() {
-			// recount max cells
-			this.anim.cellsCount = Memory.getCellsCount(this.state.cellsSize);
-
-			// rebuild memory
-			this.buildMemory();
-		},
-
-		getCellIndex: function(cellId) {
-			var rowIndex = null;
-			var colIndex = null;
-
-			// find row index
-			for (var currentRow = 1; currentRow <= this.anim.cellsCount.maxRows; currentRow++) {
-				var max = currentRow * this.anim.cellsCount.maxCols;
-
-				if (cellId < max) {
-					rowIndex = currentRow;
-					break;
-				}
-			}
-
-			// find col index
-			colIndex = cellId - (this.anim.cellsCount.maxCols * (rowIndex - 1));
-
-			return {
-				colIndex: colIndex,
-				rowIndex: rowIndex
-			};
-		},
-
-		getCellNoise: function(cellPosition) {
-			var noise = this.anim.noise;
-
-			var cellValue = noise.perlin2(cellPosition.colIndex / 15, cellPosition.rowIndex / 15);
-			var cellPercent = Math.floor(((cellValue + 1) / 2) * 100);
-
-			return cellPercent;
-		},
-
-		render: function() {
-			// build cells list
-			var cells = this.state.brainCells.map(function(cell) {
-				return (
-					<Braincell key={cell.name}
-								size={cell.size}
-								query={cell.query}
-								color={cell.color}
-								noise={cell.noise} />
-				);
-			});
-
-			return (
-				<div>
-					<div className="debug">
-						{cells.length}
-						<br/>
-						{this.anim.cellsCount.maxCols + ' x ' + this.anim.cellsCount.maxRows}
-					</div>
-
-					<ul>
-						{cells}
-					</ul>
-				</div>
-			);
-		},
-
+	var animMixin = {
 		// animation options
 		anim: {
 			// play with this!
-			randomizeMemory: 1, // randomize memory activity?
+			randomizeMemoryNoise: 1, // randomize memory activity?
 			updateMode: 0,		// 0 - full memory
 								// 1 - single memory
 
@@ -170,10 +68,124 @@ define(function(require){
 			} else {
 				this.updateMultipleMemories();
 			}
+		}
+	};
+
+	//
+	//	Cells MIXIN
+	//
+	var cellsMixin = {
+		getCellIndex: function(cellId) {
+			var rowIndex = null;
+			var colIndex = null;
+
+			// find row index
+			for (var currentRow = 1; currentRow <= this.anim.cellsCount.maxRows; currentRow++) {
+				var max = currentRow * this.anim.cellsCount.maxCols;
+
+				if (cellId < max) {
+					rowIndex = currentRow;
+					break;
+				}
+			}
+
+			// find col index
+			colIndex = cellId - (this.anim.cellsCount.maxCols * (rowIndex - 1));
+
+			return {
+				colIndex: colIndex,
+				rowIndex: rowIndex
+			};
+		},
+
+		getCellNoise: function(cellPosition) {
+			var noise = this.anim.noise;
+
+			var cellValue = noise.perlin2(cellPosition.colIndex / 15, cellPosition.rowIndex / 15);
+			var cellPercent = Math.floor(((cellValue + 1) / 2) * 100);
+
+			return cellPercent;
+		}
+	};
+
+	//
+	// Brain component
+	//
+	var Brain = React.createClass({
+		mixins: [animMixin, cellsMixin],
+
+		getInitialState: function() {
+			return {
+				// cells size
+				cellsSize: 25,
+
+				brainCells: []
+			};
+		},
+
+		componentWillMount: function() {
+			// get cells max count
+			this.anim.cellsCount = Memory.getCellsCount(this.state.cellsSize);
+
+			this.buildMemory();
+
+			// randomize memory activity
+			if (this.anim.randomizeMemoryNoise) {
+				setInterval(function() {
+					this.buildNoiseMap(this.state.brainCells);
+				}.bind(this), 7000);
+			}
+		},
+
+		componentDidMount: function() {
+			// handle window resize
+			window.addEventListener('resize', this.onScreenResize);
+
+			// start anim (rebuild memory)
+			if (this.anim.useRAF) {
+				this.redraw();
+			} else {
+				this.anim.updateTimer = setInterval(this.redraw, this.anim.updateDelay);
+			}
+		},
+
+		onScreenResize: function() {
+			// recount max cells
+			this.anim.cellsCount = Memory.getCellsCount(this.state.cellsSize);
+
+			// rebuild memory
+			this.buildMemory();
+		},
+
+		render: function() {
+			// build cells list
+			var cells = this.state.brainCells.map(function(cell) {
+				return (
+					<Braincell key={cell.name}
+								size={cell.size}
+								query={cell.query}
+								color={cell.color}
+								noise={cell.noise} />
+				);
+			});
+
+			return (
+				<div>
+					<div className="debug">
+						{cells.length}
+						<br/>
+						{this.anim.cellsCount.maxCols + ' x ' + this.anim.cellsCount.maxRows}
+					</div>
+
+					<ul>
+						{cells}
+					</ul>
+				</div>
+			);
 		},
 
 		updateSingleMemory: function() {
-			// console.log('!! updating single memory');
+			console.log('!! updating single memory');
 
 			// grab random brain cell
 			var randomCell = this.state.brainCells[Math.floor(Math.random() * this.state.brainCells.length)];
@@ -188,10 +200,10 @@ define(function(require){
 		},
 
 		updateMultipleMemories: function() {
-			var maxCells = parseInt(this.state.brainCells.length / 2);
+			var maxCells = parseInt(this.state.brainCells.length / 5);
 			var activeCells = ~~(Math.random() * maxCells) + 1;
 
-			// console.log('!! updating multiple memories ', maxCells, activeCells);
+			console.log('!! updating multiple memories ', maxCells, activeCells);
 
 			for (var i = 0; i <= activeCells; i++) {
 				// grab random brain cell
