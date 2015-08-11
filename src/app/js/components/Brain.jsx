339be625a071f70b28863bd05ebebdd669ee90ad
Sabin Tudor <sabin@gmx.us>
2015-08-11 23:35:43 +0300

    Refactor Brain component to use new Memory Entity.

--- src/app/js/components/Brain.jsx
+++ src/app/js/components/Brain.jsx
@@ -4,7 +4,6 @@ define(function(require){
 	// deps
 	var React = require('react');
 	var helpers = require('helpers');
-	var Noise = require('noise');
 
 	// models
 	var Memory = require('models/Memory');
@@ -28,83 +27,33 @@ define(function(require){
 			updateDelay: 1000,	// ignored if RAF true, slowpoke
 
 			// trackers
-			updateTimer: null,	// update timer (raf / interval)
-			cellsCount: null,	// memory cells counters (max cols / rows)
-			noise: null			// noise map
+			updateTimer: null	// update timer (raf / interval)
 		},
 
-		buildMemory: function() {
-			// build memory
-			var newMemory = Memory.createFullMemory(this.state.cellsSize, this.animation.cellsCount.maxCells);
+		update: function() {
+			// console.log('update memory!');
 
-			// add noise to memory cells
-			newMemory = this.buildNoiseMap(newMemory);
+			// snapshot current memory
+			var snapshot = this.state.memory;
 
-			// ...and update it
-			this.setState({ brainCells: newMemory });
-		},
+			// update memory
+			snapshot.update();
 
-		buildNoiseMap: function(memoryCells) {
-			// reset noise
-			this.animation.noise = new Noise(Math.random());
-
-			// add noise to memory cells
-			memoryCells.forEach(function(memoryCell, idx) {
-				var cellPosition = this.getCellIndex(memoryCell.id);
-				memoryCell.noise = this.getCellNoise(cellPosition);
-			}.bind(this));
-
-			return memoryCells;
+			this.setState({
+				memory: snapshot,
+				redrawCounter: this.state.redrawCounter + 1
+			});
 		},
 
 		redraw: function() {
+			// console.log('redraw!');
+
 			// request another frame?
 			if (this.animation.useRAF) {
 				this.animation.updateTimer = window.requestAnimFrame(this.redraw);
 			}
 
-			if (this.animation.updateMode) {
-				this.updateSingleMemory();
-			} else {
-				this.updateMultipleMemories();
-			}
-		}
-	};
-
-	//
-	//	Cells MIXIN
-	//
-	var cellsMixin = {
-		getCellIndex: function(cellId) {
-			var rowIndex = null;
-			var colIndex = null;
-
-			// find row index
-			for (var currentRow = 1; currentRow <= this.animation.cellsCount.maxRows; currentRow++) {
-				var max = currentRow * this.animation.cellsCount.maxCols;
-
-				if (cellId < max) {
-					rowIndex = currentRow;
-					break;
-				}
-			}
-
-			// find col index
-			colIndex = cellId - (this.animation.cellsCount.maxCols * (rowIndex - 1));
-
-			return {
-				colIndex: colIndex,
-				rowIndex: rowIndex
-			};
-		},
-
-		getCellNoise: function(cellPosition) {
-			var noise = this.animation.noise;
-
-			var cellValue = noise.perlin2(cellPosition.colIndex / 15, cellPosition.rowIndex / 15);
-			var cellPercent = Math.floor(((cellValue + 1) / 2) * 100);
-
-			return cellPercent;
+			this.update();
 		}
 	};
 
@@ -112,29 +61,24 @@ define(function(require){
 	// Brain component
 	//
 	var Brain = React.createClass({
-		mixins: [animMixin, cellsMixin],
+		mixins: [animMixin],
 
 		getInitialState: function() {
 			return {
-				// cells size
-				cellsSize: 25,
-
-				brainCells: []
+				memory: null,
+				redrawCounter: 0
 			};
 		},
 
 		componentWillMount: function() {
-			// get cells max count
-			this.animation.cellsCount = Memory.getCellsCount(this.state.cellsSize);
+			// create new memory
+			var newMemory = new Memory({
+				updateMode: this.animation.updateMode
+			});
 
-			this.buildMemory();
-
-			// randomize memory activity
-			if (this.animation.randomizeMemoryNoise) {
-				setInterval(function() {
-					this.buildNoiseMap(this.state.brainCells);
-				}.bind(this), 7000);
-			}
+			this.setState({
+				memory: newMemory
+			});
 		},
 
 		componentDidMount: function() {
@@ -147,78 +91,62 @@ define(function(require){
 			} else {
 				this.animation.updateTimer = setInterval(this.redraw, this.animation.updateDelay);
 			}
+
+			// randomize memory activity
+			if (this.animation.randomizeMemoryNoise) {
+				setInterval(function() {
+					this.state.memory.generateNoise();
+				}.bind(this), 6600);
+			}
 		},
 
 		onScreenResize: function() {
-			// recount max cells
-			this.animation.cellsCount = Memory.getCellsCount(this.state.cellsSize);
+			// snapshot current memory
+			var snapshot = this.state.memory;
 
-			// rebuild memory
-			this.buildMemory();
+			// rebuild/reboot memory
+			snapshot.boot();
+
+			this.setState({
+				memory: snapshot
+			});
 		},
 
 		render: function() {
-			// build cells list
-			var cells = this.state.brainCells.map(function(cell) {
-				return (
-					<Braincell key={cell.name}
-								size={cell.size}
-								query={cell.query}
-								color={cell.color}
-								noise={cell.noise} />
-				);
-			});
+			var brainCells = null;
+			var counter = null;
+
+			if (this.state.memory && this.state.memory.cells) {
+				var memory = this.state.memory;
+
+				brainCells = memory.cells.map(function(cell) {
+					return (
+						<Braincell key={cell.name}
+									size={cell.size}
+									query={cell.query}
+									color={cell.color}
+									noise={cell.noise} />
+					);
+				});
+
+				// cells counter
+				counter = brainCells.length;
+			}
 
 			return (
 				<div>
 					<div className="debug">
-						{cells.length}
-						<br/>
-						{this.animation.cellsCount.maxCols + ' x ' + this.animation.cellsCount.maxRows}
+						cells: {counter}
+						<br />
+						draw #{this.state.redrawCounter}
 					</div>
 
 					<ul>
-						{cells}
+						{brainCells}
 					</ul>
 				</div>
 			);
-		},
-
-		updateSingleMemory: function() {
-			console.log('!! updating single memory');
-
-			// grab random brain cell
-			var randomCell = this.state.brainCells[Math.floor(Math.random() * this.state.brainCells.length)];
-
-			// generate random memory
-			var newMemory = Memory.createSingleMemory();
-
-			// assign new query 'activity'
-			randomCell.query = newMemory.query;
-
-			this.setState({ brainCells: this.state.brainCells });
-		},
-
-		updateMultipleMemories: function() {
-			var maxCells = parseInt(this.state.brainCells.length / 5);
-			var activeCells = ~~(Math.random() * maxCells) + 1;
-
-			console.log('!! updating multiple memories ', maxCells, activeCells);
-
-			for (var i = 0; i <= activeCells; i++) {
-				// grab random brain cell
-				var randomCell = this.state.brainCells[Math.floor(Math.random() * this.state.brainCells.length)];
-
-				// generate random memory
-				var newMemory = Memory.createSingleMemory();
-
-				// assign new query 'activity'
-				randomCell.query = newMemory.query;
-			}
-
-			this.setState({ brainCells: this.state.brainCells });
 		}
-
 	});
 
 	// expose
