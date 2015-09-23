4140364b4a3136be55a5dd141f75b7ff65c1bc7b
Sabin Tudor <sabin@gmx.us>
2015-09-23 12:04:21 +0300

    Add basic encoder from string to decimal digits to helpers.js

--- src/app/js/common/helpers.js
+++ src/app/js/common/helpers.js
@@ -43,10 +43,38 @@ define(function(require){
 		return randomAction;
 	};
 
+	//
+	//	Basic encode of strings into decimal digits
+	//
+	var zeroPad = function(n, w) {
+		while (n.toString().length < w) {
+			n = '0' + n;
+		}
+		return n;
+	};
+
+	var toNumbers = function(str) {
+		var nums = '';
+		for (var i = 0; i < str.length; i++) {
+			nums += zeroPad(str.charCodeAt(i), 3);
+		}
+		return nums;
+	};
+
+	var fromNumbers = function(nums) {
+		var str = '';
+		for (var i = 0; i < nums.length; i += 3){
+			// console.log(i + ': ' + nums.substring(i, i + 3));
+			str += String.fromCharCode(nums.substring(i, i + 3));
+		}
+		return str;
+	};
 
 	// expose
 	return {
 		getRandomColor: _getRandomColor,
-		getRandomChar: _getRandomChar
+		getRandomChar: _getRandomChar,
+		toNumbers: toNumbers,
+		fromNumbers: fromNumbers
 	};
 });
