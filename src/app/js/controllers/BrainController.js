a476b582203cd936c9c3d58540c5625f2576f91b
Sabin Tudor <sabin@gmx.us>
2015-10-18 19:37:47 +0300

    Add animation main settings and minor bootstrap of Memory model.

--- src/app/js/controllers/BrainController.js
+++ src/app/js/controllers/BrainController.js
@@ -1,23 +1,53 @@
 define(function(require){
 	'use strict';
 
-	var BrainController = function BrainController($interval) {
+	// models
+	var Memory = require('models/Memory');
+
+	// animation options
+	var ANIMATION = {
+		// play with this!
+		alive: 1, // brain is 'alive' (randomize memory activity) ?
+		updateMode: 0,		// 0 - full memory
+							// 1 - single memory
+
+		// animation settings
+		useRAF: true,		// steroids mode on?
+		updateDelay: 1000,	// ignored if RAF true, slowpoke
+
+		// trackers
+		updateTimer: null	// update timer (raf / interval)
+	};
+
+	//
+	//	Brain component
+	//
+	var BrainController = function BrainController($scope, $timeout) {
 		var vm = this;
 
 		// ---------------
         // PUBLIC METHODS
         // ---------------
 		vm.redrawCounter = 0;
+		vm.memory = [];
 
 		// ---------------
         // PRIVATE METHODS
         // ---------------
-		var _activate = function() {
-			console.log('Brain controller up!');
 
-			$interval(function() {
-				++vm.redrawCounter;
-			}, 500);
+		var memory = null;
+
+		var _activate = function() {
+			// create new memory
+			memory = new Memory({
+				updateMode: ANIMATION.updateMode
+			});
+
+			$timeout(function() {
+				vm.memory = memory;
+
+				console.log('cells: ', vm.memory);
+			});
 		};
 
 		// ...and call it
@@ -25,7 +55,7 @@ define(function(require){
 	};
 
 	// inject modules
-	BrainController.$inject = ['$interval'];
+	BrainController.$inject = ['$scope', '$timeout'];
 
 	// expose
 	return BrainController;
