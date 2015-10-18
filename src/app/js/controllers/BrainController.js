4ed7ce90d215a7f24788c6b7bba5b01b84653fd8
Sabin Tudor <sabin@gmx.us>
2015-10-18 21:03:52 +0300

    Add stats and memory update and draw helpers to Brain controller.

--- src/app/js/controllers/BrainController.js
+++ src/app/js/controllers/BrainController.js
@@ -1,28 +1,30 @@
 define(function(require){
 	'use strict';
 
+	// deps
+	var stats = require('common/stats');
+
 	// models
 	var Memory = require('models/Memory');
 
-	// animation options
-	var ANIMATION = {
-		// play with this!
-		alive: 1, // brain is 'alive' (randomize memory activity) ?
-		updateMode: 0,		// 0 - full memory
-							// 1 - single memory
+	//
+	// #DEBUG: do you even lift, bro?
+	//
+	var benchmark = true;
 
-		// animation settings
-		useRAF: true,		// steroids mode on?
-		updateDelay: 1000,	// ignored if RAF true, slowpoke
+	// performance stats meters
+	var fpsMeter, msMeter, mbMeter = null;
 
-		// trackers
-		updateTimer: null	// update timer (raf / interval)
-	};
+	if (benchmark) {
+		fpsMeter = stats.createMeter('fps', { top: 0, left: 0 });
+		msMeter = stats.createMeter('ms', { top: 0, left: 80 });
+		// mbMeter = stats.createMeter('mb', { top: 0, left: 160 });
+	}
 
 	//
 	//	Brain component
 	//
-	var BrainController = function BrainController($scope, $timeout) {
+	var BrainController = function BrainController($scope, $timeout, $interval, raf) {
 		var vm = this;
 
 		// ---------------
@@ -31,11 +33,71 @@ define(function(require){
 		vm.redrawCounter = 0;
 		vm.memory = [];
 
+		// screen resize handler
+		vm.onScreenResize = function() {
+			console.log('resize bro!!!!!!');
+		};
+
 		// ---------------
         // PRIVATE METHODS
         // ---------------
-
 		var memory = null;
+
+		// animation options
+		var ANIMATION = {
+			// play with this!
+			alive: 1, // brain is 'alive' (randomize memory activity) ?
+			updateMode: 0,		// 0 - full memory
+								// 1 - single memory
+
+			// animation settings
+			useRAF: true,		// steroids mode on?
+			updateDelay: 1000,	// ignored if RAF true, slowpoke
+
+			// trackers
+			updateTimer: null	// update timer (raf / interval)
+		};
+
+		//
+		//	update memory
+		//
+		var update = function() {
+			// console.log('update memory!');
+			// $timeout(function() {
+			memory.update();
+			++vm.redrawCounter;
+			// });
+			$scope.$digest();
+		};
+
+		//
+		//	redraw
+		//
+		var redraw = function() {
+			// console.log('redraw!');
+
+			// request another frame?
+			if (ANIMATION.useRAF) {
+				ANIMATION.updateTimer = raf(redraw);
+			}
+
+			// benchmark start
+			if (benchmark) {
+				fpsMeter.begin();
+				msMeter.begin();
+				// mbMeter.begin();
+			}
+
+			// update & render
+			update();
+
+			// benchmark end
+			if (benchmark) {
+				fpsMeter.end();
+				msMeter.end();
+				// mbMeter.end();
+			}
+		};
 
 		var _activate = function() {
 			// create new memory
@@ -43,11 +105,23 @@ define(function(require){
 				updateMode: ANIMATION.updateMode
 			});
 
-			$timeout(function() {
-				vm.memory = memory;
+			// set memory
+			vm.memory = memory;
 
-				console.log('cells: ', vm.memory);
-			});
+			// start animation (rebuild memory)
+			if (ANIMATION.useRAF) {
+				ANIMATION.updateTimer = raf(redraw);
+			} else {
+				ANIMATION.updateTimer = setInterval(redraw, ANIMATION.updateDelay);
+			}
+
+			// randomize memory activity
+			if (ANIMATION.alive) {
+				setInterval(function() {
+					console.log('gen noise!');
+					memory.generateNoise();
+				}, 6000);
+			}
 		};
 
 		// ...and call it
@@ -55,7 +129,7 @@ define(function(require){
 	};
 
 	// inject modules
-	BrainController.$inject = ['$scope', '$timeout'];
+	BrainController.$inject = ['$scope', '$timeout', '$interval', 'RAF'];
 
 	// expose
 	return BrainController;
