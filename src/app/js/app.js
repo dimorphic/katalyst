364b987a5b570bb35c38bd42168caea8c5b9d01f
Sabin Tudor <sabin@gmx.us>
2015-10-24 16:43:24 +0300

    Cleanup main app.js

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -3,51 +3,36 @@ define(function(require){
 
 	// deps
 	var $ = require('jquery');
-	var helpers = require('common/helpers');
-
-	require('annyang');
 
 	// components
 	var Brain = require('components/Brain');
-	var Ear = window.annyang;
-	var app = null;
+	var Ear = require('components/Ear');
 
+	// pointers
+	var APP, EAR;
+
+	//
 	// VOICE COMMANDS
+	//
 	var COMMANDS = {
 		'hello *term': function(term) {
-			var translated = helpers.toNumbers(term);
-
-			// strip leading zeros
-			while ((parseInt(translated.charAt(0), 10) === 0)) {
-				translated = translated.slice(1);
-			}
-
-			// make the brain think about the translated msg
-			app.think(translated);
+			// interpret user term / query
+			APP.interpret(term);
 		}
 	};
 
-	Ear.addCommands(COMMANDS);
-
-	// #debug
-	Ear.addCallback('resultMatch', function(userSaid, cmdText, phrases) {
-		console.log('user said:', userSaid, '(', cmdText, ')');
-		console.log('phrases:', phrases);
-		console.log(' ');
-	});
-
+	//
 	// DOM ready
+	//
 	$(function() {
-		var $bttn = $('#boot');
-
 		// initialize Brain
-		app = new Brain('#app');
+		APP = new Brain('#app');
+		EAR = new Ear(COMMANDS, { autoRestart: false });  // should be true
 
-		$bttn.on('click', function() {
-			// app.render();
-			// app.think(68097118101);
-			// #debug: init Ear
-			Ear.start({ autoRestart: false, continuous: true });
+		// restart listening for voice inputs (mic tends to fail sometimes on local)
+		// note: need HTTPS to stop the browser for asking mic permissions
+		$('#boot').on('click', function() {
+			EAR.listen();
 		});
 	});
 });
