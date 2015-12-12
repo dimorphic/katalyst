f04e0c49280f45f4bfaa9de8aeb5e567a24016dd
Sabin Tudor <sabin@gmx.us>
2015-12-12 15:58:57 +0200

    Major refactor to Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,118 @@
 // var CONFIG = require('../config');
 
 // deps
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
-// get environment
-var ENV = process.env.NODE_ENV || 'development';
+// settings
+const HOST = 'localhost';
+const PORT = 9090;
+
+// get env
+const ENV = process.env.NODE_ENV || 'development';
+
+const ENV_CONFIG = {
+	development: {
+		js: '[name].js',
+		css: '[name].css',
+		img: '[path][name].[ext]'
+	},
+
+	production: {
+		js: '[name]-[hash].js',
+		css: '[name]-[hash].css',
+		img: '[path][name]-[hash].[ext]'
+	}
+};
+
+// get config based on ENV
+const CONFIG = ENV_CONFIG[ENV];
 
 // paths
-var PATHS = {
-	src: path.join(__dirname, '/src/app'),
-	build: path.join(__dirname, '/dist')
+const PATHS = {
+	app: path.join( __dirname, 'src/app' ),
+	public: path.join( __dirname, 'src/www' ),
+	build: path.join( __dirname, 'dist' )
 };
+
+// webpack plugins
+const PLUGINS = [
+	new webpack.DefinePlugin({
+		'__DEV__': JSON.stringify(ENV === 'development' || false),
+		'process.env': { 'NODE_ENV': JSON.stringify(process.env.NODE_ENV) }
+	}),
+	new webpack.HotModuleReplacementPlugin(),
+	new webpack.NoErrorsPlugin() // don't emit assets on errors
+];
+
+// code transform loaders (@TODO: move to own config file?)
+const LOADERS = [
+	// ES6 transformer
+	{
+		test: /\.js$|.jsx$/,
+		loaders: ['babel?presets[]=es2015'],
+		include: `${PATHS.app}/js/`,
+		exclude: [
+			path.join( __dirname, 'node_modules' ),
+			path.join( __dirname, 'bower_components' )
+		]
+	}
+];
 
 // module
 module.exports = {
-	context: PATHS.src,
+	context: PATHS.app,
 
-	entry: {
-		app: 'main.js'
-	},
+	entry: [
+		'webpack/hot/dev-server',
+		`webpack-dev-server/client?http://${HOST}:${PORT}`,
+		`${PATHS.app}/js/main.js`
+	],
 
 	output: {
-		path: PATHS.build,
-		publicPath: '/assets/',
-		filename: 'bundle.js'
+		path: `${PATHS.build}/dev`,
+		publicPath: '/',
+		filename: CONFIG.js
+	},
+
+	// misc
+	cache: (ENV === 'development'),
+	debug: (ENV === 'development'),
+	devtool: (ENV === 'development') ? 'eval' : '',
+
+	// plugins & loaders
+	plugins: PLUGINS,
+	module: { loaders: LOADERS	},
+
+	resolve: {
+		root: [ PATHS.app ],
+		extensions: [
+			'',
+			'.js',
+			'.jsx',
+			'.css',
+			'.scss',
+		]
+	},
+
+	// DEV server (@TODO: move to own module?)
+	devServer: {
+		host: HOST,
+		port: PORT,
+
+		contentBase: PATHS.public,
+
+		hot: true,
+		inline: true,
+		progress: true,
+
+		historyApIFallback: true,
+
+		stats: {
+			colors: true,
+			version: false,
+			timings: true
+		}
 	}
 };
 
